e177fd50e1baec0c55d60a1133f3d53ca25b9868
agusnieto77 <agustin.nieto77@gmail.com>
2024-12-14 19:24:34 -0300

    up

--- main_lp_0.py
+++ main_lp_0.py
@@ -2,29 +2,29 @@ import os
 import json
 from config_lp_0 import procesar_archivo
 
-nombre_archivo = './txt/lp/1880_03_04_BUE_LP_U_00_000_MasterLimpio.txt'
+directorio_entrada = './txt/lp'
 directorio_salida = './json'
 
 def main():
-    ruta_absoluta = os.path.abspath(nombre_archivo)
-    nombre_base = os.path.basename(ruta_absoluta)
-
     try:
-        with open(ruta_absoluta, 'r', encoding='utf-8') as file:
-            contenido = file.read()
-            resultados = procesar_archivo(nombre_base, contenido)
-
         os.makedirs(directorio_salida, exist_ok=True)
-        nombre_salida = f"{os.path.splitext(nombre_base)[0]}_procesado.json"
-        ruta_salida = os.path.join(directorio_salida, nombre_salida)
-        
-        with open(ruta_salida, 'w', encoding='utf-8') as f:
-            json.dump(resultados, f, ensure_ascii=False, indent=2)
-        
-    except FileNotFoundError:
-        print(f"Error: No se pudo encontrar el archivo '{nombre_archivo}'")
+
+        for nombre_archivo in os.listdir(directorio_entrada):
+            if nombre_archivo.endswith('.txt'):
+                ruta_absoluta = os.path.join(directorio_entrada, nombre_archivo)
+
+                with open(ruta_absoluta, 'r', encoding='utf-8') as file:
+                    contenido = file.read()
+                    resultados = procesar_archivo(nombre_archivo, contenido)
+
+                nombre_salida = f"{os.path.splitext(nombre_archivo)[0]}_procesado.json"
+                ruta_salida = os.path.join(directorio_salida, nombre_salida)
+                
+                with open(ruta_salida, 'w', encoding='utf-8') as f:
+                    json.dump(resultados, f, ensure_ascii=False, indent=2)
+
     except Exception as e:
-        print(f"Error al procesar el archivo: {str(e)}")
+        print(f"Error al procesar los archivos: {str(e)}")
 
 if __name__ == "__main__":
     main()
